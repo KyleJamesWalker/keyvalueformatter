c718025a30ca6f31f3cb763d71dadb50a62682b3
Steven Liu <steven.liu@zefr.com>
2015-11-10 17:34:47 -0800

    Fix typo

--- tests/test_log.py
+++ tests/test_log.py
@@ -52,20 +52,20 @@ class LogTestCase(unittest.TestCase):
 
         if "u'" in self.buffer.getvalue():
             self.assertEqual(self.buffer.getvalue(),
-                            '''dict="{u'hello': u'world'}"'''
-                            ''';=field="True"'''
-                            ''';list="[u'nice', u'time']"'''
-                            ''';message="a message"'''
-                            ''';more="More goes here"'''
-                            '''\n''')
+                             '''dict="{u'hello': u'world'}"'''
+                             ''';field="True"'''
+                             ''';list="[u'nice', u'time']"'''
+                             ''';message="a message"'''
+                             ''';more="More goes here"'''
+                             '''\n''')
         else:
             self.assertEqual(self.buffer.getvalue(),
-                            '''dict="{'hello': 'world'}"'''
-                            ''';field="True"'''
-                            ''';list="['nice', 'time']"'''
-                            ''';message="a message"'''
-                            ''';more="More goes here"'''
-                            '''\n''')
+                             '''dict="{'hello': 'world'}"'''
+                             ''';field="True"'''
+                             ''';list="['nice', 'time']"'''
+                             ''';message="a message"'''
+                             ''';more="More goes here"'''
+                             '''\n''')
 
 if __name__ == '__main__':
     unittest.main()
